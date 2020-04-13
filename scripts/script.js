ab5cfc3eb8d28c13e27268482c5e279e623f26cd
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-13 23:40:30 +0300

    13 домашка

---
+++ scripts/script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+let todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed'),
+    btn = document.querySelector('.header-button');
+
+let todoData = [];
+let keys = Object.keys(localStorage);
+
+for(let key of keys) {
+    todoData.push({
+        value: key,
+        completed: localStorage.getItem(key)
+    });
+}
+
+function render() {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(function (item, index) {
+        let li = document.createElement('li');
+
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' + 
+                        '<div class="todo-buttons">' +
+                            '<button class="todo-remove"></button>' +
+                            '<button class="todo-complete"></button>' +
+                        '</div>';
+                        
+        if(item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        let btnTodoCompleted = li.querySelector('.todo-complete'),
+            btnRemove = li.querySelector('.todo-remove');
+
+        btnTodoCompleted.addEventListener('click', function() {
+            item.completed = !item.completed;
+
+            localStorage.setItem(item.value, item.completed);
+
+            render();
+        });
+
+        btnRemove.addEventListener('click', function(index) {
+            localStorage.removeItem(item.value);
+
+            todoData.splice(index, 1);
+
+            render();
+        });
+    });
+};
+
+btn.disabled = true;
+
+headerInput.addEventListener('input', function(event) {
+    if(event.target.value === '') {
+        btn.disabled = true;
+    } else {
+        btn.disabled = false;
+    }
+});
+
+todoControl.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+
+    localStorage.setItem(newTodo.value, newTodo.completed);
+
+    todoData.push(newTodo);
+
+    headerInput.value = '';
+    btn.disabled = true;
+
+    render();
+});
+
+if(todoData.length > 0) {
+    render();
+}
